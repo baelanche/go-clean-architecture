bc8691fa77d1a4215491279d88048541f34bb380
unknown <baelanche@naver.com>
2024-05-28 19:42:32 +0900

    remove domain

--- internal/domain/article.go
+++
@@ -1,9 +0,0 @@
-package domain
-
-type Article struct {
-	Title     string `json:"title"`
-	Body      string `json:"body"`
-	UserName  string `json:"userName"`
-	CreatedAt string `json:"createdAt"`
-	UpdatedAt string `json:"updatedAt"`
-}
